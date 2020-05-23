2c961cfe2805671982f0e2f132c64e987d398ac2
jeremiassilvaroit <jeremiassilvaroit@bitbucket.org>
2020-05-23 14:39:20 -0300

    Lint

--- src/ExpressMiddleware.ts
+++ src/ExpressMiddleware.ts
@@ -1,12 +1,12 @@
 import { ModelMapper } from "./ModelMapper";
-import { Request, Response, NextFunction } from 'express'
+import { Request, NextFunction } from 'express'
 
 
 export interface ModelMapperRequest extends Request {
     mapper: ModelMapper
 }
 
-export function modelMapperMiddleware (req: ModelMapperRequest, res: Response, next: NextFunction) {
+export function modelMapperMiddleware (req: ModelMapperRequest, {}, next: NextFunction) {
     const map = new ModelMapper()
     map.setJsonObject(req.body)
     req.mapper = map

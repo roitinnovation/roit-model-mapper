3a4e2a2f9b45274fadbccd1fa56a885c93661034
jeremiassilvaroit <jeremiassilvaroit@bitbucket.org>
2021-09-09 17:40:12 -0300

    remove express mid

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import 'reflect-metadata'
-export { modelMapperMiddleware } from "./ExpressMiddleware"
-export { ModelMapperRequest } from "./ExpressMiddleware"
 export { ModelMapper } from "./ModelMapper"
 export { ObjectMapperOptions } from "./ObjectMapperOptions"
 export { JsonProperty } from "./JsonProperty"
